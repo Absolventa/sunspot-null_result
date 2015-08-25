e4e5b52789d6991e14ea9b7abd369163670c0a93
Carsten Zimmermann <cz@aegisnet.de>
2015-08-25 17:21:59 +0200

    Return the sunspot pagination interface

--- lib/sunspot/null_result.rb
+++ lib/sunspot/null_result.rb
@@ -2,6 +2,32 @@ require "sunspot/null_result/version"
 
 module Sunspot
   class NullResult
-    # Your code goes here...
+
+    class PaginatedNullArray < Array
+      def total_count
+        0
+      end
+
+      def current_page
+        1
+      end
+
+      def total_pages
+        1
+      end
+
+      def limit_value
+        1
+      end
+    end
+
+    def hits
+      PaginatedNullArray.new
+    end
+
+    def results
+      PaginatedNullArray.new
+    end
+
   end
 end
--- spec/sunspot/null_result_spec.rb
+++ spec/sunspot/null_result_spec.rb
@@ -1,11 +1,36 @@
 require 'spec_helper'
 
-describe Sunspot::NullResult do
+RSpec.describe Sunspot::NullResult do
   it 'has a version number' do
     expect(Sunspot::NullResult::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  shared_examples_for 'returns a paginated enumerable' do |method|
+    subject { described_class.new.send(method) }
+
+    it 'returns a total count of 0' do
+      expect(subject.total_count).to eql 0
+    end
+
+    it 'returns a total page count of 1' do
+      expect(subject.total_pages).to eql 1
+    end
+
+    it 'returns the current page as 1' do
+      expect(subject.current_page).to eql 1
+    end
+
+    it 'returns the limit value as 1' do
+      expect(subject.limit_value).to eql 1
+    end
   end
+
+  describe '#hits' do
+    it_behaves_like 'returns a paginated enumerable', :hits
+  end
+
+  describe '#results' do
+    it_behaves_like 'returns a paginated enumerable', :results
+  end
+
 end
