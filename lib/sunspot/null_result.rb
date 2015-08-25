ed98212416eb0ae60a583db52027f6d65375e092
Carsten Zimmermann <cz@aegisnet.de>
2015-08-25 12:30:33 +0200

    Add skeleton by bundler

---
+++ lib/sunspot/null_result.rb
@@ -0,0 +1,7 @@
+require "sunspot/null_result/version"
+
+module Sunspot
+  module NullResult
+    # Your code goes here...
+  end
+end
---
+++ lib/sunspot/null_result/version.rb
@@ -0,0 +1,5 @@
+module Sunspot
+  module NullResult
+    VERSION = "0.1.0"
+  end
+end
